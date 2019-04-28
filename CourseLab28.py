69f57117309f63da86c14f5e52fd981d85951232
zambrzycki-zbigniew <zambrzycki.zbigniew@gmail.com>
2019-04-28 11:01:05 +0200

    Course 28

---
+++ CourseLab28.py
@@ -0,0 +1,17 @@
+# calculating the area of a circle
+piValue = 3.14
+circleRadius = 5
+a = 5
+b = 10
+h = 2
+circleArea = piValue*circleRadius**2
+print("Area of the circle with radius of ", circleRadius, " is: ", circleArea)
+# calculating the circumference of a circle
+circleCircumference = 2*piValue*circleRadius
+print("Circumference of the circle with radius of ", circleRadius, " is: ", circleCircumference)
+# calculating the area of a rectangle
+rectangleArea = a*b
+print("The area of rectangle with sides equal to ", a, "and ", b, " is: ", rectangleArea)
+# calculating the area of a trapezoid
+trapezoidArea = (a+b)*h/2
+print("The area of trapezoid with sides equal to ", a, ", ", b, " and height equal to", h, " is: ", trapezoidArea)
